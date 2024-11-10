e26617dc297d219224575d562cb4cffca2eb16c8
Aaron Rhoades <aaronrhoades91@gmail.com>
2024-11-09 19:24:21 -0500

    create pages, services, and add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { PageComponent } from './page/page.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 
+        path: '',
+        component: PageComponent,
+    },
+    {
+        path: 'blog',
+        component: PostsComponent,
+    },
+    {
+        path: 'blog/:slug',
+        component: PostComponent,
+    },
+    { 
+        path: ':slug',
+        component: PageComponent,
+    },
+
+];
